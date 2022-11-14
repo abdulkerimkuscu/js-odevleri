8f9170e40d175b9fcc15eb281610cca6315e036c
Abdülkerim Kuşcu <kuscu2708@gmail.com>
2022-11-14 14:57:16 +0300

    local stroage added

---
+++ to-do/aa.js
@@ -0,0 +1,122 @@
+let items;
+const inputDOM = document.querySelector("#task");
+const listDOM = document.querySelector("#list");
+const btnDOM = document.querySelector("#liveToastBtn");
+const delAllBtnDOM = document.querySelector("#deleteAll");
+eventListener();
+loadItem();
+
+
+
+
+function eventListener() {
+    btnDOM.addEventListener("click", addTodo);
+    listDOM.addEventListener("click", deleteItem)
+    delAllBtnDOM.addEventListener("click", deleteAllItem)
+}
+function loadItem() {
+    items = getItemsFromLS()
+
+    items.forEach((item) => {
+        createItems(item)
+    });
+
+}
+
+
+function getItemsFromLS() {
+    if (localStorage.getItem("items") === null) {
+        items = [];
+    }
+    else {
+        items = JSON.parse(localStorage.getItem("items"));
+    }
+    return items;
+}
+
+function setItemToLS(text) {
+    items = getItemsFromLS();
+    items.push(text);
+    localStorage.setItem("items", JSON.stringify(items));
+}
+
+function deleteItemFromLS(text) {
+    items = getItemsFromLS();
+
+    items.forEach(function (item, index) {
+        if (item === text) {
+            items.splice(index, 1);
+        }
+    });
+    localStorage.setItem("items", JSON.stringify(items));
+}
+
+function addTodo() {
+    let toDo = inputDOM.value
+    if (toDo === "") {
+        $(".error").toast("show");
+    }
+    else {
+        $(".success").toast("show");
+        createItems(toDo);
+        setItemToLS(toDo);
+
+    }
+
+
+}
+function deleteItem(e) {
+    if (e.target.className === "close")
+        e.target.parentElement.parentElement.remove();
+
+    console.log(e.target.parentElement)
+    deleteItemFromLS(e.target.parentElement.parentElement.textContent)
+}
+
+listDOM.addEventListener("click", function (item) {
+
+    if (item.target.tagName = "li") {
+        item.target.classList.toggle("checked")
+    }
+    toggleDeleteBTN()
+
+
+});
+
+function toggleDeleteBTN() {
+    let checkList = document.querySelectorAll(" .checked")
+    if (checkList.length > 0) {
+        document.querySelector("#deleteAll").classList.remove("d-none")
+
+    }
+    else {
+        document.querySelector("#deleteAll").classList.add("d-none")
+    }
+
+}
+function deleteAllItem() {
+    let element = document.querySelectorAll(".checked")
+    console.log(element);
+    element.forEach(function (item) {
+        item.remove();
+
+    })
+    
+
+    delAllBtnDOM.classList.add("d-none")
+}
+
+function createItems(text) {
+
+    const liDOM = document.createElement("li");
+    listDOM.appendChild(liDOM);
+    liDOM.appendChild(document.createTextNode(text));
+    inputDOM.value = ""
+    // close butonu
+    let closeSpan = document.createElement("span");
+    let closeBtn = document.createElement("img");
+    closeBtn.setAttribute("src", "/icon.svg")
+    closeBtn.className = "close";
+    liDOM.appendChild(closeSpan);
+    closeSpan.appendChild(closeBtn)
+}
--- to-do/script.js
+++ to-do/script.js
@@ -1,76 +1,110 @@
-let doList = ["3 Litre Su İç","En Az 3 Saat Kodlama Yap","Yemek Yap"];
-let ulListDOM = document.querySelector("#list");
-
-// Listeye item ekleme
-doList.forEach(function(item){
-    createItem(item);
-   
-});
-
-// Listeyi check etme ve hepsini sil butonunu çağırma fonkisiyonu 
-ulListDOM.addEventListener("click", function(item){
-
-    if(item.target.tagName= "li"){
-        item.target.classList.toggle("checked")
-    }
-    toggleDeleteBTN()
-
-});
-
-// Liste Check olunca hepsini sil butonunun gözükmesi
-function toggleDeleteBTN(){
-    let checkList = document.querySelectorAll(" .checked")
-    if(checkList.length > 0){
-        document.querySelector("#deleteAll").classList.remove("d-none")
-        
-    }
-    else{
-        document.querySelector("#deleteAll").classList.add("d-none")
-    }
-}
-
-// Check listin hepsinin silinme fonsiyonu
-document.querySelector("#deleteAll").onclick = function(){
-   let element = document.querySelectorAll(".checked")
-   
-   element.forEach(function(item){
-    item.remove();
-   })
-}
-
-// Listeye eleman ekleme ayrıca boş ekleme ve eklendikten sonra uyaarıların gösterilmesi 
-document.querySelector("#liveToastBtn").onclick = function(){
-    let doList = document.querySelector("#task").value;
-    if(doList == ""){
-        $(".error").toast("show")
-    }
-    else{
-        $(".success").toast("show")
-        createItem(doList)
-        document.querySelector("#task").value = "";
-    }
+// let doList;
+// let ulListDOM = document.querySelector("#list");
+// document.addEventListener('DOMContentLoaded',getItemFromLS)
+// // Listeye item ekleme
+// function loadItem(){
     
-}
+//     doList = getItemFromLS()
+//     doList.forEach(function(item){
+//     createItem(item);
+     
+//     });
+// }
+// function getItemFromLS(){
+//     if(localStorage.getItem("doList") === null){
+//         doList = [];
+//     }
+//     else{
+//             doList = JSON.parse(localStorage.getItem("doList"));
+//     }
+//     return doList;
+// }
 
-// Listeye eleman ekleme liste içindeki silme butonu eklenmesi ve silme butonun fonsiyonu
-function createItem(item){
-    // listeye eleman ekleme başlangıcı
-    let liDOM = document.createElement("li");
-    let text = document.createTextNode(item)
-    liDOM.className = "list-group-item"
-    liDOM.appendChild(text);
-    ulListDOM.appendChild(liDOM);
-    // listeye eleman ekleme sonu
+// function setItemToLS(text){
+//     doList = getItemFromLS();
+//     doList.push(text);
+//     localStorage.setItem("doList", JSON.stringify(doList))
+// }
 
-    // çarpı butonu ekleme 
-    let closeBtn= document.createElement("span");
-    let closeText= document.createTextNode("\u00D7");
-    closeBtn.className= "close";
-    closeBtn.appendChild(closeText);
-    liDOM.appendChild(closeBtn);
-    // çarpı butonu fonksiyonu
-    closeBtn.onclick = function(){
-    this.parentElement.remove();
-    }
+// function deleteItemFromLS(text){
+//     doList = getItemFromLS();
 
-}
+//     doList.forEach(function(item , index){
+//         if(item === text){
+//             item.splice(index,1)
+//         }
+//     });
+//     localStorage.setItem("doList" ,JSON.stringify(doList))
+// }
+// // Listeyi check etme ve hepsini sil butonunu çağırma fonkisiyonu 
+// ulListDOM.addEventListener("click", function(item){
+
+//     if(item.target.tagName= "li"){
+//         item.target.classList.toggle("checked")
+//     }
+//     toggleDeleteBTN()
+    
+// });
+
+// // Liste Check olunca hepsini sil butonunun gözükmesi
+// function toggleDeleteBTN(){
+//     let checkList = document.querySelectorAll(" .checked")
+//     if(checkList.length > 0){
+//         document.querySelector("#deleteAll").classList.remove("d-none")
+        
+//     }
+//     else{
+//         document.querySelector("#deleteAll").classList.add("d-none")
+//     }
+// }
+
+// // Check listin hepsinin silinme fonsiyonu
+// document.querySelector("#deleteAll").onclick = function(){
+//    let element = document.querySelectorAll(".checked")
+   
+//    element.forEach(function(item){
+//     item.remove();
+//     localStorage.clear(".checked")
+//    })
+   
+// }
+
+// // Listeye eleman ekleme ayrıca boş ekleme ve eklendikten sonra uyaarıların gösterilmesi 
+// document.querySelector("#liveToastBtn").onclick = function(){
+//     let toDoList = document.querySelector("#task").value;
+//     if(toDoList == ""){
+//         $(".error").toast("show")
+//     }
+//     else{
+//         $(".success").toast("show")
+//         setItemToLS(toDoList)
+//         createItem(toDoList)
+//         document.querySelector("#task").value = "";
+//     }
+    
+// }
+
+// // Listeye li elemanı, silme butonu eklenmesi ve silme butonun fonsiyonu
+// function createItem(item){
+//     // listeye eleman ekleme başlangıcı
+//     let liDOM = document.createElement("li");
+//     let text = document.createTextNode(item)
+//     liDOM.className = "list-group-item"
+//     liDOM.appendChild(text);
+//     ulListDOM.appendChild(liDOM);
+    
+//     // listeye eleman ekleme sonu
+
+//     // çarpı butonu ekleme 
+//     let closeBtn= document.createElement("span");
+//     let closeText= document.createTextNode("\u00D7");
+//     closeBtn.className= "close";
+//     closeBtn.appendChild(closeText);
+//     liDOM.appendChild(closeBtn);
+//     // çarpı butonu fonksiyonu
+//     closeBtn.onclick = function(){
+//     this.parentElement.remove();
+//     deleteItemFromLS(this.parentElement.textContent)
+//     }
+
+// }
